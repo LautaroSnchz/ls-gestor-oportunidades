cc039929c426ad56fb981cf22be369d33f0e8ac0
LautaroSnchz <lautaro.snchz94@gmail.com>
2026-04-29 22:51:17 -0300

    chore: inicialización del plugin LS Gestor de Oportunidades

---
+++ README.md
@@ -0,0 +1,2 @@
+# LS Gestor de Oportunidades
+Plugin de arquitectura avanzada para WordPress. Expone un sistema de gestión mediante CPTs y WP REST API.
---
+++ ls-gestor-oportunidades.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: LS Gestor de Oportunidades
+ * Description: Sistema avanzado de gestión mediante CPTs y WP REST API.
+ * Version: 1.0.0
+ * Author: Lautaro
+ * Text Domain: ls-gestor-oportunidades
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
